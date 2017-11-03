d4aebabf3dd657658541fa87b1a6b2d3afb9d57a
Tyler <trsman44@gmail.com>
2017-11-03 13:12:27 -0400

    Updated the StoredKeys and AsymmetricKey classes to be fully functional.

--- src/AsymmetricKey.java
+++ src/AsymmetricKey.java
@@ -1,8 +1,20 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 /**
  * @Authors Tyler, Matt, Daniel
- * @Date_Updated 11/1/17
+ * @Date_Updated 11/3/17
  * @Model_Used Strategy
  * 
  * This is used for Asymmetric Key. The plan is to have the user be able to choose what type of encryption / decryption
@@ -10,15 +22,98 @@ import java.io.File;
  */
 public class AsymmetricKey implements FileCryptoInterface
 {
-    @Override
-    public File fileEncryptor(File file, String algorithm, StoredKeys keys) //The encrypt key is the public key
+	//Initializing the cipher
+    private Cipher cipher;
+    
+    //Constructor that sets the instance of the cipher to be RSA
+    public AsymmetricKey() throws NoSuchAlgorithmException, NoSuchPaddingException
     {
-        return null; //Needs to be changed
+    		cipher = Cipher.getInstance("RSA");
     }
 
     @Override
-    public File fileDecryptor(File file, String algorithm, StoredKeys keys) //The decrypt key is the private key
+    public File fileEncryptor(File file, String algorithm, StoredKeys keys) 
     {
-        return null; //Needs to be changed
+    		//Creates the file where the encrepted message will be stored
+        File encryptFile = new File("Messages/Encrypted-Message.txt");
+        
+        //Initializing the byte array
+        byte[] fileBytes;
+		try
+		{
+			//Makes the file an array of bytes
+			fileBytes = getFileInBytes(file);
+			
+			//Encrypts the file
+			encryptFile(fileBytes, encryptFile, keys.getPrivateKey());
+		} catch (IOException | GeneralSecurityException e)
+		{
+			e.printStackTrace();
+		}
+		
+		//Returns the encrypted file
+        return encryptFile;
+    }
+
+    @Override
+    public File fileDecryptor(File file, String algorithm, StoredKeys keys) 
+    {
+    		//Creates the file where the decrypted message will be stored
+        File decryptFile = new File("Messages/Decrypted-Message.txt");
+
+        //Initializing the byte array
+        byte[] fileBytes;
+		try
+		{
+			//Make the file an array of bytes
+			fileBytes = getFileInBytes(file);
+			
+			//Decrypts the file
+			decryptFile(fileBytes, decryptFile, keys.getPublicKey());
+		} catch (IOException | GeneralSecurityException e)
+		{
+			e.printStackTrace();
+		}
+
+		//Returns the decrypted file
+        return decryptFile;
+    }
+
+    //Function that gets the file bytes then returns them back
+    public byte[] getFileInBytes(File file) throws IOException
+    {
+        FileInputStream fis = new FileInputStream(file);
+        byte[] fbytes = new byte[(int) file.length()];
+        fis.read(fbytes);
+        fis.close();
+        return fbytes;
+    }
+
+    //Function that Encrypts the file
+    public void encryptFile(byte[] input, File output, PrivateKey key)
+            throws IOException, GeneralSecurityException
+    {
+        this.cipher.init(Cipher.ENCRYPT_MODE, key);
+        writeToFile(output, this.cipher.doFinal(input));
+    }
+
+    //Function that Decrypts the file
+    public void decryptFile(byte[] input, File output, PublicKey key)
+            throws IOException, GeneralSecurityException
+    {
+        this.cipher.init(Cipher.DECRYPT_MODE, key);
+        writeToFile(output, this.cipher.doFinal(input));
+    }
+
+    //Function that writes the bytes to the file specified then returns the file
+    private File writeToFile(File output, byte[] toWrite)
+            throws IllegalBlockSizeException, BadPaddingException, IOException
+    {
+        FileOutputStream fos = new FileOutputStream(output);
+        fos.write(toWrite);
+        fos.flush();
+        fos.close();
+
+        return output;
     }
 }
--- src/StoredKeys.java
+++ src/StoredKeys.java
@@ -1,12 +1,19 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 /**
  * @Authors Tyler, Matt, Daniel
- * @Date_Updated 11/1/17
+ * @Date_Updated 11/3/17
  * @Model_Used Singleton
  * 
  * This is a class used to keep track of the keys. This will make it so the encryption and decryption classes
@@ -15,6 +22,7 @@ import java.security.PublicKey;
 public class StoredKeys
 {
 	private static StoredKeys instance;
+
 	private String SymmetricKey;
 	private PublicKey publicKey;
 	private PrivateKey privateKey;
@@ -25,7 +33,7 @@ public class StoredKeys
 	private StoredKeys() { }
 	
 	public static StoredKeys getInstance()
-	{
+    {
 		if(instance == null)
 			instance = new StoredKeys();
 		return instance;
@@ -72,11 +80,57 @@ public class StoredKeys
 	{
 		//Uses RSA to generate the key pair
 		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-	    kpg.initialize(512); // 512 is the keysize.
-	    
-	    KeyPair kp = kpg.generateKeyPair();
-	    
-	    publicKey = kp.getPublic();
-	    privateKey = kp.getPrivate();
+
+		try
+		{
+			//Initializes the key size
+			kpg.initialize(1024);
+
+			//Makes the key pair
+			KeyPair kp = kpg.generateKeyPair();
+
+			//Saves the public and private keys to be used for encryption and decryption
+			publicKey = kp.getPublic();
+			privateKey = kp.getPrivate();
+
+			//This is where we plan to store the keys.
+			writeToFile("KeyPair/publicKey", getPublicKey().getEncoded());
+			writeToFile("KeyPair/privateKey", getPrivateKey().getEncoded());
+		} catch (IOException e)
+		{
+			System.err.println(e.getMessage());
+		}
+	}
+
+	//Function that saves the keys to a file so they can be used another time
+	public void writeToFile(String path, byte[] key) throws IOException
+	{
+		File f = new File(path);
+		f.getParentFile().mkdirs();
+
+		FileOutputStream fos = new FileOutputStream(f);
+		fos.write(key);
+		fos.flush();
+		fos.close();
+	}
+	
+	//Function that gets the private key from a file specified
+	public PrivateKey getPrivateFromFile(String filename) throws Exception
+	{
+		byte[] keyBytes = Files.readAllBytes(new File(filename).toPath());
+		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		
+		return kf.generatePrivate(spec);
+	}
+
+	//Function taht gets the public key from a file specified
+	public PublicKey getPublicFromFile(String filename) throws Exception
+	{
+		byte[] keyBytes = Files.readAllBytes(new File(filename).toPath());
+		X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		
+		return kf.generatePublic(spec);
 	}
 }
