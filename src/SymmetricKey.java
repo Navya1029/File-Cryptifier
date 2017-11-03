b81f602550c7cbd6fb09f8fea208aba62d024814
Thesargent95 <33225943+Thesargent95@users.noreply.github.com>
2017-11-03 13:26:24 -0400

    Modified Encrypt method in AsymmetricKey Class to preform Decrypt a previously encrypted file
    Signed-off-by: Thesargent95 <33225943+Thesargent95@users.noreply.github.com>

--- src/SymmetricKey.java
+++ src/SymmetricKey.java
@@ -12,7 +12,7 @@ import sun.misc.BASE64Decoder;
 
 /**
  * @Authors: Tyler, Matt, Daniel
- * @Date Updated: 11/2/17
+ * @Date Updated: 11/3/17
  * @Model_Used: Strategy
  *
  * This is used for Symmetric Key. The plan is to have the user be able to choose what type of encryption / decryption
@@ -31,13 +31,13 @@ public class SymmetricKey implements FileCryptoInterface
     			//This is the type of cipher we will use
 				Cipher c = Cipher.getInstance("AES");
 
-				//Takes the key and converts it to something we can use to encode
+				//Takes the key and converts it to something we can use to decode
 				byte[] key = keys.getSymmetricKey().getBytes("UTF-8");
 				MessageDigest sha = MessageDigest.getInstance("SHA-1");
 				key = sha.digest(key);
 				key = Arrays.copyOf(key, 16);
 
-				SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+				SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES"); //Creates a secret key
 
 				//Creates the cipher in encrypt mode using this key
 				c.init(Cipher.ENCRYPT_MODE, secretKeySpec);
@@ -48,7 +48,7 @@ public class SymmetricKey implements FileCryptoInterface
 				StringBuffer sb = new StringBuffer(); //A string buffer
 				for(int i = 0; i < cipherText.length; i++)
 				{
-					sb.append(Integer.toString((cipherText[i] & 0xff) + 0x100, 16).substring(1)); //puts the encrypted stuff in the buffer
+					sb.append(Integer.toString((cipherText[i] & 0xff) + 0x100, 16).substring(1)); //puts the decrypted stuff in the buffer
 				}
 
 				FileWriter fw = new FileWriter(tempFile); //Writes files
@@ -56,13 +56,45 @@ public class SymmetricKey implements FileCryptoInterface
 				fw.close(); //Closes the file
 		} catch (Exception e) { System.out.println("AES Encryption Failed"); }
 		
-        return tempFile; //Needs to be changed
+        return tempFile;
     }
 
     @Override
     public File fileDecryptor(File file, String algorithm, StoredKeys keys)
     {
-        return null; //Needs to be changed
+		File tempFile = new File("AES-Decrypted");
+
+		try
+		{
+			//This is the type of cipher we will use
+			Cipher c = Cipher.getInstance("AES");
+
+			//Takes the key and converts it to something we can use to encode
+			byte[] key = keys.getSymmetricKey().getBytes("UTF-8");
+			MessageDigest sha = MessageDigest.getInstance("SHA-1");
+			key = sha.digest(key);
+			key = Arrays.copyOf(key, 16);
+
+			SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+
+			//Creates the cipher in decrypt mode using this key
+			c.init(Cipher.DECRYPT_MODE, secretKeySpec);
+
+			//Encrypts the files bytes
+			byte[] cipherText = c.doFinal(Files.readAllBytes(file.toPath()));
+
+			StringBuffer sb = new StringBuffer(); //A string buffer
+			for(int i = 0; i < cipherText.length; i++)
+			{
+				sb.append(Integer.toString((cipherText[i] & 0xff) + 0x100, 16).substring(1)); //puts the encrypted stuff in the buffer
+			}
+
+			FileWriter fw = new FileWriter(tempFile); //Writes files
+			fw.write(sb.toString()); //writes to file
+			fw.close(); //Closes the file
+		} catch (Exception e) { System.out.println("AES Decryption Failed"); }
+
+		return tempFile;
     }
     
     private Key generateKeyFromString(final String secKey) throws Exception
